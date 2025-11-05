b840d4820e46a5fd5aaf8bc9b6853f2a2a2b01ff
Ray <46488172+vuvantrong@users.noreply.github.com>
2025-11-05 18:48:24 +0700

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,17 +1,15 @@
 // ==UserScript==
-// @name         Rophim Full VIP
+// @name         VIP Script
 // @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  Bypass VIP + Coin trên Rophim
-// @author       FireT
-// @require      https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/rophim_vip.js
-// @homepageURL   https://github.com/firetofficial/bypass-rophim-vip
-// @downloadURL   https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
-// @updateURL     https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
+// @version      1.0
+// @description  Script test qua GitHub
+// @author       Raydz
+// @require      https://raw.githubusercontent.com/vuvantrong/vip/refs/heads/main/vip.js
 // @match        *://www.rophim.me/*
 // @match        *://www.rophim.mx/*
 // @match        *://www.rophim.li/*
+// @updateURL    https://raw.githubusercontent.com/vuvantrong/vip/refs/heads/main/tampermonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/vuvantrong/vip/refs/heads/main/tampermonkey.user.js
 // @run-at       document-start
 // @grant        none
-// @icon         https://avatars.githubusercontent.com/u/120646974?s=48&v=4
 // ==/UserScript==
