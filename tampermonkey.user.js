3116742ae2008ec4461abe26cfbb958bf4562561
Ray <46488172+vuvantrong@users.noreply.github.com>
2025-11-05 19:14:02 +0700

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -4,12 +4,12 @@
 // @version      1.0
 // @description  Script test qua GitHub
 // @author       Raydz
-// @require      https://raw.githubusercontent.com/vuvantrong/vip/refs/heads/main/vip.js
 // @match        *://www.rophim.me/*
 // @match        *://www.rophim.mx/*
 // @match        *://www.rophim.li/*
-// @updateURL    https://raw.githubusercontent.com/vuvantrong/vip/refs/heads/main/tampermonkey.user.js
-// @downloadURL  https://raw.githubusercontent.com/vuvantrong/vip/refs/heads/main/tampermonkey.user.js
+// @require      https://raw.githubusercontent.com/vuvantrong/vip/main/vip.js
+// @updateURL    https://raw.githubusercontent.com/vuvantrong/vip/main/tampermonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/vuvantrong/vip/main/tampermonkey.user.js
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
