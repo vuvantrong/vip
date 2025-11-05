7db4aac7256837617ecd5d45225503e9624287a2
Ray <46488172+vuvantrong@users.noreply.github.com>
2025-11-05 21:01:42 +0700

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VIP Script
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Script test qua GitHub
 // @author       Raydz
 // @match        *://www.rophim.me/*
